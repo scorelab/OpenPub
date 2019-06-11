a33ecb53109630d8681f26bea7dc5fffccddf7c9
RODRIGO-GSOC <rodrigo-gsoc@Roshans-MacBook-Air.local>
2019-06-12 00:54:20 +1000

    added publications route

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var app = express();
 var port = 3000;
 var ip = '127.0.0.1';
 
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.send("This will be the landing page");
+    res.render("home");
 });
+
+app.get("/publication",function(req,res){
+    res.render("publication");
+});
+
 
 app.listen(port,ip,function(){
     console.log("Program server has started!");
