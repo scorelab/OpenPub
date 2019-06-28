0672f21c7519240bb4920d05dd761c2af12d309d
RODRIGO-GSOC <rodrigo-gsoc@Roshans-MacBook-Air.local>
2019-06-28 18:30:36 +1000

    fix mongo network error

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -10,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/auth/google">Sign in with Google</a>
       </header>
     </div>
   );
