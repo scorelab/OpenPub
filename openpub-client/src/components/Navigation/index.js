fcedae6d9963b9c31aa05567acf0b8fc4fbbf03d
delimaGo <melol6515@gmail.com>
2019-07-13 19:37:20 +0530

    Add Navigation Bar

--- openpub-client/src/components/Navigation/index.js
+++ openpub-client/src/components/Navigation/index.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #455A64;
+  }
+
+  a, .navbar-brand, .navbar-nav .nav-link {
+    color: #f4f5f7;
+
+    &:hover {
+      color: white;
+    }
+  }
+`;
 
 class Navigation extends Component {
   render() {
     return (
-        <div>
-          <h2>Navigation</h2>
-        </div>
+      <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">OpenPub</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+          <Nav.Item>
+              <Nav.Link>
+                <Link to="/add-publication">+ Add Publication</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/profile">Profile</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/about">About</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/signin">SignIn</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/signup">SignUp</Link>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles >
     );
   }
 }
