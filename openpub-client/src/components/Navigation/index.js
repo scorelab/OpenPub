8ddcc9a8c3e9a134a055900d3494fbf7de170063
delimaGo <melol6515@gmail.com>
2019-07-15 23:54:54 +0530

    Update Nav Bar

--- openpub-client/src/components/Navigation/index.js
+++ openpub-client/src/components/Navigation/index.js
@@ -9,8 +9,12 @@ const Styles = styled.div`
     background-color: #455A64;
   }
 
+  .navbar-light .navbar-brand {
+    color: white;
+}
+
   a, .navbar-brand, .navbar-nav .nav-link {
-    color: #f4f5f7;
+    color: white;
 
     &:hover {
       color: white;
@@ -22,40 +26,40 @@ class Navigation extends Component {
   render() {
     return (
       <Styles>
-      <Navbar expand="lg">
-        <Navbar.Brand href="/">OpenPub</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-          <Nav.Item>
-              <Nav.Link>
-                <Link to="/add-publication">+ Add Publication</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/profile">Profile</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/about">About</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/signin">SignIn</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/signup">SignUp</Link>
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Styles >
+        <Navbar expand="lg">
+          <Navbar.Brand href="/">OpenPub</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/add-publication">+ Add Publication</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/profile">Profile</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/about">About</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/signin">SignIn</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/signup">SignUp</Link>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Styles >
     );
   }
 }
