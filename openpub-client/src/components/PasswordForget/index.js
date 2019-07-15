fef15b31c014da1a4bd803a3a4b58b5d2a38ff99
delimaGo <melol6515@gmail.com>
2019-07-15 23:56:25 +0530

    Add rest password page

--- openpub-client/src/components/PasswordForget/index.js
+++ openpub-client/src/components/PasswordForget/index.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 class PasswordForget extends Component {
   render() {
     return (
-        <div>
-          <h2>PasswordForget</h2>
+      <div className="modal-body align-w3">
+      <form action="#" method="post" className="p-sm-3">
+        <div className="form-group">
+          <label htmlFor="email" className="col-form-label">Email</label>
+          <input type="text" className="form-control" placeholder="Email" name="email" id="email"
+            required="" />
         </div>
+        <div className="right-w3l">
+          <a href="/reset-password" className="form-control bg-theme" id="register_txt" value=""><p align="center">Rest Passsword</p></a>
+        </div>
+        <p className="text-center dont-do text-secondary">Don't have an account?
+            <a href="/signup" className="text-theme-2 font-weight-bold">
+            Sign up Now!!!</a>
+        </p>
+      </form>
+    </div>
     );
   }
 }
